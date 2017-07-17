6ed0a2fe7ae075ccf669776c34fc96884af1a9df
Gilberto Silveira Jr <junior.gilberto@gmail.com>
2017-07-16 21:56:38 -0300

    Inclusão do servidor estatico para teste

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 Está atividade consiste na criação de um [boilerplate](https://pt.stackoverflow.com/questions/10575/o-que-%C3%A9-boilerplate-code), com as ferramentas apresentadas no modulo.
 
 
+## Plugins utilizados
+
+* [gulp](https://www.npmjs.com/package/gulp);
+* [gulp-htmlmin](https://www.npmjs.com/package/gulp-htmlmin);
+* [gulp-sass](https://www.npmjs.com/package/gulp-sass);
+* [gulp-connect](https://www.npmjs.com/package/gulp-connect)
+
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlmin = require('gulp-htmlmin');
+var connect = require('gulp-connect')
 
 gulp.task('sass',function(){
     gulp.src('./source/scss/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css/'))
+    .pipe(connect.reload())
 })
 
 gulp.task('htmlmin',function(){
     gulp.src('./source/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
 })
 
 gulp.task('watch', function(){
     gulp.watch(['./source/*.html'],['htmlmin'])
     gulp.watch(['./source/scss/*.scss'],['sass'])
 })
+
+gulp.task('server',function(){
+    connect.server({
+        root : './dist',
+        livereload : true
+    })
+})
+
+gulp.task('dev',['server','watch'])
